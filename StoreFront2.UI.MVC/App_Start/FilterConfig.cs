a1919ee9b0dc4b7b641a7e6f34cb6600e8dfdbd3
Fishe90 <dillon.f617@gmail.com>
2021-03-11 15:07:31 -0600

    Initial Commit + Identity Samples

---
+++ StoreFront2.UI.MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace StoreFront2.UI.MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
